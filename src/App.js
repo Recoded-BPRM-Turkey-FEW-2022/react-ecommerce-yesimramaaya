e08ef295c5234591d071eab03f8d4203cea938d5
AyaMidani <61182291+AyaMidani@users.noreply.github.com>
2022-06-25 01:16:51 +0300

    Categories is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { ReactQueryDevtools } from 'react-query/devtools'
 import "./style.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider} from 'react-query';
+import AllProducts from "./routes/AllProducts";
+import Cart from "./routes/Cart";
+import Product from "./routes/Product";
+import NotFound from "./routes/NotFound"
+import Filter from "./components/Filter";
+import Categories from './components/Categories';
+import Navbar from "./components/Navbar";
 
+const queryClient = new QueryClient()
 export default function App() {
   return (
-  
-  <></>
+  <QueryClientProvider client={queryClient}>
+   <BrowserRouter>
+   <div>
+     <Navbar />
+   </div>
+   <Routes>
+   <Route path="/products/" element={<AllProducts />} />
+   <Route path="/cart/" element={<Cart />}/>
+   <Route path="/products/:id" element={<Product />} />
+   <Route path="/categories/:id" element={<Categories />}/>
+   <Route  element={<NotFound />} />
+   </Routes>
+   </BrowserRouter>
+   <ReactQueryDevtools initialIsOpen={false} />
+   </QueryClientProvider>
   );
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+import { React, useState } from "react";
+
+function Search({query}) {
+  console.log(query)
+  return (
+    <div>
+     <h1></h1> 
+    </div>
+  );
+}
+
+export default Search;
